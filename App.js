bf6c92605df08113a834ab678c039a69d92b363a
Admin <neillonwork@gmail.com>
2024-07-02 19:29:42 -0300

    criado função handleAdd

--- App.js
+++ App.js
@@ -10,14 +10,24 @@ import {
 import { FontAwesome } from "@expo/vector-icons";
 
 export default function App() {
-  
+  const [tarefa, setTarefa] = useState("");
+
+  function handleAdd() {
+    alert(tarefa);
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tarefas</Text>
       <View style={styles.containerInput}>
-        <TextInput placeholder="Digite sua tarefa" style={styles.input} />
+        <TextInput
+          placeholder="Digite sua tarefa"
+          style={styles.input}
+          value={tarefa}
+          onChangeText={(text) => setTarefa(text)}
+        />
 
-        <TouchableOpacity style={styles.buttonAdd}>
+        <TouchableOpacity style={styles.buttonAdd} onPress={handleAdd}>
           <FontAwesome name="plus" size={20} color="#fff" />
         </TouchableOpacity>
       </View>
